8eb4338fc18fc8eabf32e4489bd054a69ba632e8
Alex Camacho <alex.c@WalkMe.local>
2018-08-06 13:12:44 -0700

    user is able to select a product id and quantity and order the items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -20,7 +21,38 @@ const afterConnection = function () {
         response.forEach(item => {
             console.log("Item ID: " + item.item_id + ", Name: " + item.product_name + ", Price: $" + item.price);
         })
+        promptUser();
     });
 
-    connection.end();
+
+}
+
+const promptUser = function () {
+    inquirer.prompt([{
+            type: "prompt",
+            message: "What is the ID of the product you'd like to buy?",
+            name: "p_id"
+        },
+        {
+            type: "prompt",
+            message: "What quantity of the product would you like to buy?",
+            name: "quantity"
+        }
+    ]).then(answers => {
+        connection.query('SELECT * FROM products WHERE item_id = ' + answers.p_id, function (error, response) {
+            if (error) console.log(error);
+            let stock = response[0].stock_quantity;
+            let user_quantity = parseInt(answers.quantity);
+            if (stock >= user_quantity) {
+                connection.query("UPDATE products SET stock_quantity = " + (stock - user_quantity) + " WHERE item_id = " + answers.p_id, function (error, response2) {
+                    if (error) console.log(error);
+                    console.log("Total cost: $" + (response[0].price * user_quantity));
+                    connection.end();
+                })
+            } else {
+                console.log("Insufficient quantity!");
+                connection.end();
+            }
+        })
+    })
 }
